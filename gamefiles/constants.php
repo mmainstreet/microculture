65cc8249680d75c9fc8439441b83d121898a9d26
Heeiiigou <schoerschle1@freenet.de>
2015-11-02 21:16:03 +0100

    Deleted everything

--- gamefiles/constants.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("MAXCELLCOUNT",	"5");
-define("ATTRIBUTECOUNT","10");
-define("AREAX",			"800");
-define("AREAY",			"600");
-?>
--- js/variables.js
+++
@@ -1,8 +0,0 @@
-var team1 = new Array();
-var team2 = new Array();
-		
-for(var i = 0;i<MAXCELLCOUNT; i++)
-{
-  team1[i] = new Array(10);
-  team2[i] = new Array(10);
-}
